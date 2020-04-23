bd77f0bb4bd689c46e67fd57f78b81a9058ed030
achyutbiz <achyutbiz@gmail.com>
2020-04-24 03:16:21 +0800

    world map deploy

--- app.py
+++ app.py
@@ -101,10 +101,10 @@ def messages():
 
 if __name__ == '__main__':
 
-   HOST = os.environ.get('SERVER_HOST', 'localhost')
-   try:
-       PORT = int(os.environ.get('SERVER_PORT', '5555'))
-   except ValueError:
-        PORT = 5555
-   app.run(HOST, PORT)
+   #HOST = os.environ.get('SERVER_HOST', 'localhost')
+   #try:
+   #    PORT = int(os.environ.get('SERVER_PORT', '5555'))
+   #except ValueError:
+    #    PORT = 5555
+   #app.run(HOST, PORT)
    app.run()
